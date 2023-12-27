5f65fa05bdc70567735e524cbaa364676f80dcdf
kostas <kmcartelle@gmail.com>
2023-12-27 01:01:25 -0300

    lo que me costo que aparerasca el fantasma LPM!

--- backend/server/server.js
+++ backend/server/server.js
@@ -4,9 +4,16 @@ const cors = require('cors')
 
 const app = express();
 const PORT = 3000;
+
+const KAE = "e0b045";
+const KAF = "e0b046";
+const KAFf = "KAFf";
+const KAEf = "KAEf";
+
 app.use(cors())
 
 let jsonData = null;
+let vuelosGuardados = {};
 
 // Middleware para analizar el cuerpo de las solicitudes JSON
 app.use(bodyParser.json());
@@ -15,6 +22,15 @@ app.use(bodyParser.json());
 // Ruta para recibir el JSON por GET
 app.get('/vuelos', (req, res) => {
   if (jsonData) {
+    if (!jsonData.hasOwnProperty(KAE) && vuelosGuardados.hasOwnProperty(KAEf)){
+      jsonData[KAEf] = vuelosGuardados[KAEf];
+      //jsonData["pepeE"] = vuelosGuardados[KAE];
+    }
+    if (!jsonData.hasOwnProperty(KAF) && vuelosGuardados.hasOwnProperty(KAFf)){
+      jsonData[KAFf] = vuelosGuardados[KAFf];
+      //jsonData["pepeF"] = [0,1,2,3];
+    }
+
     res.json(jsonData);
   } else {
     res.status(404).json('sin datos');
@@ -23,27 +39,31 @@ app.get('/vuelos', (req, res) => {
 
 // Ruta para enviar el JSON por POST y almacenarlo en memoria
 app.post('/vuelos', (req, res) => {
+    //jsonData = null;
     jsonData = req.body;
 
-    //var KAE = "e0b045";
-    var KAE = "a12d2f";
-    var KAF = "e0b046";
-
     if (jsonData.hasOwnProperty(KAE)){
-        datosKAE = [];
-        datosKAE = jsonData[KAE];
-        datosKAE[17] = new Date();
-        jsonData['LVKAEf'] = jsonData[KAE];
-        //console.log(jsonData['LVKAEf']);
+      jsonData[KAE].push(new Date());
+      //vuelosGuardados.KAEf = [...jsonData[KAE]];
+      vuelosGuardados.KAEf = [...jsonData[KAE]];
+      console.log("KAE presente");
     }
     if (jsonData.hasOwnProperty(KAF)){
-        datosKAE = [];
-        datosKAE = jsonData[KAF];
-        datosKAE[17] = new Date();
-        jsonData['LVKAFf'] = jsonData[KAF];
-        //console.log(jsonData['LVKAFf']);
+      jsonData[KAF].push(new Date());
+      vuelosGuardados.KAFf = [...jsonData[KAF]];
+      console.log("KAF presente");
     }
-    console.log(jsonData)
+    for (let key in jsonData) {
+      if (
+        jsonData.hasOwnProperty(key) &&
+        Array.isArray(jsonData[key]) &&
+        (jsonData[key][1] === 0 || jsonData[key][2] === 0)
+      ) {
+        delete jsonData[key];
+      }
+    }
+
+    console.log("cantidad de vuelos en seguimiento " + Object.keys(jsonData).length);
     res.json({ message: 'Datos recibidos y almacenados en memoria' });
 });
 
